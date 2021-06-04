76383c3e5c4a67207e51c9403a10ffa1401c0d78
Dan DeLorenzo <dan@dgfx.us>
2021-06-04 10:39:06 -0400

    inti2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    var svg = d3.select("svg");
+    var svg = d3.select("#stacksvg");
 
     var svgJq = $("#stacksvg");
 
@@ -24,16 +24,20 @@ $( document ).ready(function() {
         console.log(event);
     })
 
-        mapboxgl.accessToken = 'pk.eyJ1IjoiY2l6emxlIiwiYSI6ImNrcDJ0MjhteTE5cGsyb213bms0dHp6c3QifQ.-dc9k9y6KKnDlE5UszjS9A';
-        var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/cizzle/ckp5swexc0t0k17qcolpk960i',
-        center: [-74.033216, 40.716560], // starting position [lng, lat]
-        zoom: 17, // starting zoom
-        bearing: -44.68, //bearing
-        pitch: 50.50,
-        interactive: false
-        });
+
+
+    ////MAPBOX
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2l6emxlIiwiYSI6ImNrcDJ0MjhteTE5cGsyb213bms0dHp6c3QifQ.-dc9k9y6KKnDlE5UszjS9A';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/cizzle/ckp5swexc0t0k17qcolpk960i',
+    center: [-74.033216, 40.716560], // starting position [lng, lat]
+    zoom: 17, // starting zoom
+    bearing: -44.68, //bearing
+    pitch: 50.50,
+    interactive: false
+    });
 
 
 });
