c7c5d757cbd68beabc11e8c402bb0c45367a47d8
Dan DeLorenzo <dan@dgfx.us>
2021-06-04 10:19:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+//     var svg = d3.select("svg");
+
+//     var svgJq = $("#stacksvg");
+
+//     var suites = svg.selectAll(".suites");
+    
+//     //suites.style("visibility", "hidden");
+
+//     suites.on("mouseover", function(event, d) {
+//         //d3.select(this).selectAll(".suite").classed("suite-hover", true);
+//         d3.select(this).selectAll(".suite").style("fill-opacity", 1);
+//         console.log(this);
+//     })
+
+
+//     suites.on("mouseleave", function(event, d) {
+//         d3.select(this).selectAll(".suite").style("fill-opacity", .25);
+
+//     })
+
+
+//     suites.on("click", function(event, d) {
+//         console.log(event);
+//     })
+
+
+
+// });
